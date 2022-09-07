8b70e3c277593b76a67338467bc0c1b6979a9b28
Ola <ohagenbendixen@gmail.com>
2022-09-07 11:36:49 +0200

    Create divide function

--- script.js
+++ script.js
@@ -10,3 +10,10 @@ function multiply(num, num2) {
     return num * num2;
 }
 
+function divide(num, num2) {
+    if(num == 0 || num2 == 0) {
+        return 'Error'
+    }
+    return num / num2;
+}
+
